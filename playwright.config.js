0ac4adf18cdc97fc3ad93c6812ee9b587eaf7e5b
ersu-amine <ersu.amine@gmail.com>
2025-02-28 01:17:36 -0500

    setup

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,6 @@
-// @ts-check
+
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -45,16 +38,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    //commente out for learning purposes. firefox and webkit
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit', //for safari browser
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit', //for safari browser
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
