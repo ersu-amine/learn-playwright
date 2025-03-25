f7e4fa241affc4d84a87b26375e74c5f2af5b37b
ersu-amine <ersu.amine@gmail.com>
2025-03-25 00:43:27 -0400

    practice with key press and hover

--- tests/practice3/practiceTestGroup2.test.js
+++ tests/practice3/practiceTestGroup2.test.js
@@ -8,39 +8,61 @@ test.describe('Test for internet page', () => {
         context = await browser.newContext();
         page = await context.newPage(); //opens empty tab
         await page.setViewportSize({ width: 1920, height: 1080 });
-        await page.goto('https://the-internet.herokuapp.com'); //opens in another tab
-               
 
+        await page.goto('https://the-internet.herokuapp.com');
     });
 
-    test('@smoke key pressing test', async ({ page }) => {
+    test('@smoke key pressing test', async () => {
+
         const element = page.locator("//a[contains(text(),'Key Presses')]");
 
+
         // Scroll to key press links
-        await element.scrollIntoViewIfNeeded();//does not scroll because of new empty tab
+        await element.scrollIntoViewIfNeeded();
+        await page.mouse.wheel(0, 300);
+        await page.waitForTimeout(1000);
 
         // Open the key presses page
         await element.click();
+        await page.waitForTimeout(1000);
 
         // Locate input box
         let inputBox = page.locator("//input[@id='target']");
+        await page.waitForTimeout(1000);
 
         // Fill in the input box with 'Hello'
-        await inputBox.fill('Hello');
+        await inputBox.fill("Hello");
+        await page.waitForTimeout(1000);
 
-        // Confirm the last key pressed is 'o'
-        // Add your assertion here
+
+        // Confirm the last the value entered is 'Hello'
+
+
+        // Confirm the value in the input box is cleared
+        let expectedValue = "Hello";
+        //assertion
+        await expect(await inputBox.inputValue(), "Expect the input value to be Hello").toBe(expectedValue);
+
     });
 
-    test('Test2', async ({ page }) => {
+    test('Hover test', async () => {
+        // Test implementation
+        const element = page.locator("//*[text()='Hovers']");
+        await element.click();
+        await page.waitForTimeout(1000);
+
+        const middleUser = page.locator("(//img[@alt='User Avatar'])[2]");
+        await middleUser.hover();
+        await page.waitForTimeout(1000);
+
+
+    });
+
+    test('Test3', async () => {
         // Test implementation
     });
 
-    test('Test3', async ({ page }) => {
-        // Test implementation
-    });
-
-    test('Test4', async ({ page }) => {
+    test('Test4', async () => {
         // Test implementation
     });
 
